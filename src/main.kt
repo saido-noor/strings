5ebaf8161894309e3f5b3860e24460553c11b5d4
saido-noor <saidoomarnoor@gmail.com>
2022-04-13 09:44:53 +0300

    Strings

---
+++ src/main.kt
@@ -0,0 +1,74 @@
+fun main(){
+//    var name = "Banana"
+//    print(name.first())
+//    var name1 = name[2]
+//    var name2 = name[3]
+//    var name3 = name[4]
+//    var result = "$name1$name2$name3"
+//    println(result)
+
+//   var result = name[2].toString() + name[3].toString() + name[4].toString()
+//    println(result)
+//
+//    var x = "a"
+//    var y = "b"
+//    var comb = x.toString() + y
+//    println(comb)
+
+//    var names = "Bob"
+//    println(names.decapitalize())
+//    println(names.capitalize())
+//    println(names.uppercase())
+//    var names1 = "Bob"
+//  println(names=="Bob")
+//    println(names.equals("Bob"))
+
+
+//var holiday = "December"
+//    println(holiday[0])
+//    println(holiday[1])
+//    println(holiday[2])
+//    println(holiday[3])
+//    println(holiday[4])
+//    println(holiday[5])
+//    println(holiday[6])
+//    println(holiday[7])
+
+
+//    var name = "saido"
+//    var greeting = "hello $name how are you"
+//    println(greeting)
+//    var stmt = "Today is the"
+//    var position = "last"
+//    var stmt2 = "Saturday of february"
+//    var result = "$stmt $position $stmt2"
+//    println(result)
+//    var x =""
+//    var y = " "
+//
+//    println(x.isEmpty())
+//    println(x.isBlank())
+//    println(y.isEmpty())
+//    println(y.isBlank())
+//     var president = " Yuweri Museveni "
+//    println(president)
+//    println(president.trim())
+//    println(president.trimStart())
+//    println(president.trimEnd())
+//    println(president.startsWith("Y"))
+    var sentence = "Kenya is a beautifull country"
+    var words = sentence.split(" ")
+    println(words[0])
+    println(words[1])
+    println(words[2])
+    println(words[3])
+
+
+
+//    println(sentence.replace(" a "," x "))
+
+
+
+
+
+}
